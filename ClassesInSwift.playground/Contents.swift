1041f1eb1833ae151a068513c9c3e4c51b5e586a
Alejandro <AlexMJHS@gmail.com>
2015-12-07 09:20:50 -0700

    Playground added to repo

---
+++ ClassesInSwift.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var notes = "Using classes in Swift"
+
+class SimpleClass
+{
+    //Declaration section
+    var name:String
+    var age:Int
+    var isTired :Bool
+    
+    init()
+    {
+        self.name = String()
+        self.age = -99
+        self.isTired = true
+    }
+    
+    convenience init(name :String)
+    {
+        self.init()
+        self.name = name
+    }
+    
+    convenience init(name :String, age :Int)
+    {
+        self.init()
+        self.name = name
+        self.age = age
+    }
+}
+var someInstance = SimpleClass()
+print(someInstance.name)
+var otherInstance = SimpleClass(name: "derf")
+print(otherInstance.name)
+var convenienceInstance = SimpleClass(name: "other", age: 4567)
+print(convenienceInstance.age)
---
+++ SwiftJava.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var Java : [String]
+Java = [String] ()
+var Str = "Hi, this is my third swift program"
+print("Hi there screen")
+Java.append("It is pretty silly")
+print("Zack lives in Draper")
+print("Jordan likes pizza")
+print(5*1)
+print(5*5)
